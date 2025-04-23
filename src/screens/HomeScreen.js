b117399bdcd0c25200202d17ffffa8394c606cc3
Augusto <augustoortolan02@gmail.com>
2025-04-23 20:07:10 -0300

    home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 import PokemonCard from "../components/PokemonCard";
 import { mockPokemon } from "../data/pokemonData";
 
+import axios from "axios";
+
 export default function HomeScreen({ navigation }) {
+  const [pokemons, setPokemons] = useState([]);
+
+  async function fetchPokemons() {
+    try {
+      const response = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=200&offset=0"
+      );
+      setPokemons(response.data.results);
+    } catch (error) {
+    } finally {
+    }
+  }
+
+  function extractPokemonId(url) {
+    const parts = url.split("/");
+    return parts[parts.length - 2];
+  }
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
   return (
     <FlatList
-      data={mockPokemon}
+      data={pokemons}
       numColumns={2}
-      keyExtractor={(item) => item.id.toString()}
+      keyExtractor={(item) => item.url}
       renderItem={({ item }) => (
         <PokemonCard
-          pokemon={item}
+          pokemon={{ ...item, id: extractPokemonId(item.url) }}
           onPress={() => navigation.navigate("Detalhes", { pokemon: item })}
         />
       )}
